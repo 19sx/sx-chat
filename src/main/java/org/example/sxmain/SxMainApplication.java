a9a9633e5198a83465ef37808373523363f6aeaa
19sx <alexanderherman270@gmail.com>
2026-07-02 19:03:48 +0200

    Hello Offline World!

---
+++ src/main/java/org/example/sxmain/SxMainApplication.java
@@ -0,0 +1,13 @@
+package org.example.sxmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SxMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sxmain/SxMainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sxmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SxMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
